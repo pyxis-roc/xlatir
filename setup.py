ee7dc72e9f6d2ad995554f841049c1a47163bd5a
sree <sree@buckyball>
2021-01-14 15:52:26 -0500

    Add scripts to setup.py, so they will be installed in the binary directory

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from setuptools import setup, find_packages
 setup(name='xlatir',
       version='0.1',
       packages=find_packages(),
+      scripts=['bin/imp2func_ssa.py',
+               'bin/xir2x.py',
+               'bin/xirconvert.py']
 )
